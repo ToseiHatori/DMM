096412a707debad457573a67f60077b15678377c
Tosei Hatori <tosei.hatori@gmail.com>
2017-12-23 12:32:30 +0900

    結果を見たりするところを修正

--- script/04-w2v.R
+++ script/04-w2v.R
@@ -14,21 +14,11 @@ require(purrr)
 
 # 名前のデータと分かち書きしたタイトルのデータから学習----------
 params <- expand.grid(
-  vectors = c(10, 100)
-  , windows = c(15, 50)
+  vectors = c(10, 100, 300, 500)
+  , windows = c(5, 15, 30)
 )
 for(loop_i in 1 : nrow(params)){
-  # 名前の部分を学習
-  output_file_names <- paste0("./model/model_names_v", params$vectors[loop_i], "_w", params$windows[loop_i], ".txt")
-  try(
-    wordVectors::train_word2vec(
-      train_file = "./out/names_list.csv", output_file = output_file_names,
-      vectors = params$vectors[loop_i], window = params$windows[loop_i],
-      threads = 3, force = T
-    )
-    , silent = TRUE
-  )
-
+  print(paste("now is ", params$vectors[loop_i], "vec", params$windows[loop_i], "win"))
   # タイトルの部分を学習
   output_file_titles <- paste0("./model/model_titles_v", params$vectors[loop_i], "_w", params$windows[loop_i], ".txt")
   try(
@@ -43,105 +33,181 @@ for(loop_i in 1 : nrow(params)){
 
 
 # 結果を見てみる------
-title_model_v10_w15 <- read.binary.vectors(filename = "./model/model_titles_v10_w15.txt")
-title_model_v10_w50 <- read.binary.vectors(filename = "./model/model_titles_v10_w50.txt")
-title_model_v100_w15 <- read.binary.vectors(filename = "./model/model_titles_v100_w15.txt")
-title_model_v100_w50 <- read.binary.vectors(filename = "./model/model_titles_v100_w50.txt")
+
+
+# ちょっとした関数定義
+queryToMat <- function(word, window_size, vector_size){
+  title_model_1 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[1],"_w", window_size[1],".txt"))
+  title_model_2 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[2],"_w", window_size[2],".txt"))
+  title_model_3 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[3],"_w", window_size[3],".txt"))
+  
+  tmp_mat <- matrix(nrow = 10, ncol = 6)
+  tmp_mat[, 1] <- nearest_to(title_model_1, word) %>% names()
+  tmp_mat[, 2] <- nearest_to(title_model_1, word) %>% unlist() %>% round(digits = 2)
+  tmp_mat[, 3] <- nearest_to(title_model_2, word) %>% names()
+  tmp_mat[, 4] <- nearest_to(title_model_2, word) %>% unlist() %>% round(digits = 2)
+  tmp_mat[, 5] <- nearest_to(title_model_3, word) %>% names()
+  tmp_mat[, 6] <- nearest_to(title_model_3, word) %>% unlist() %>% round(digits = 2)
+  return(rbind(c("w=5", "", "w=15", "", "w=30", ""), rep(x = c("word", "distance"), 3), tmp_mat))
+}
 
 # 名詞
-nearest_to(title_model_v10_w15, "アナル")
-nearest_to(title_model_v10_w50, "アナル")
-nearest_to(title_model_v100_w15, "アナル")
-nearest_to(title_model_v100_w50, "アナル")
-nearest_to(title_model_v10_w15, "おっぱい")
-nearest_to(title_model_v10_w50, "おっぱい")
-nearest_to(title_model_v100_w15, "おっぱい")
-nearest_to(title_model_v100_w50, "おっぱい")
-nearest_to(title_model_v10_w15, "うんこ")
-nearest_to(title_model_v10_w50, "うんこ")
-nearest_to(title_model_v100_w15, "うんこ")
-nearest_to(title_model_v100_w50, "うんこ")
-nearest_to(title_model_v10_w15, "コスプレ")
-nearest_to(title_model_v10_w50, "コスプレ")
-nearest_to(title_model_v100_w15, "コスプレ")
-nearest_to(title_model_v100_w50, "コスプレ")
-nearest_to(title_model_v10_w15, "妄想")
-nearest_to(title_model_v10_w50, "妄想")
-nearest_to(title_model_v100_w15, "妄想")
-nearest_to(title_model_v100_w50, "妄想")
+mat_1 <- rbind(
+  queryToMat(word = "アナル", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "アナル", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "アナル", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "アナル", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+  )
+View(mat_1)
+
+mat_1 <- rbind(
+  queryToMat(word = "眼鏡", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "眼鏡", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "眼鏡", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "眼鏡", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+mat_1 <- rbind(
+  queryToMat(word = "おっぱい", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "おっぱい", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "おっぱい", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "おっぱい", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+mat_1 <- rbind(
+  queryToMat(word = "うんこ", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "うんこ", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "うんこ", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "うんこ", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+
 # やや抽象的な言葉
-nearest_to(title_model_v10_w15, "解禁")
-nearest_to(title_model_v10_w50, "解禁")
-nearest_to(title_model_v100_w15, "解禁")
-nearest_to(title_model_v100_w50, "解禁")
-nearest_to(title_model_v10_w15, "開発")
-nearest_to(title_model_v10_w50, "開発")
-nearest_to(title_model_v100_w15, "開発")
-nearest_to(title_model_v100_w50, "開発")
+mat_1 <- rbind(
+  queryToMat(word = "解禁", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "解禁", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "解禁", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "解禁", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+mat_1 <- rbind(
+  queryToMat(word = "開発", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "開発", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "開発", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "開発", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+nearest_to(title_model_1, "解禁")
+nearest_to(title_model_2, "解禁")
+nearest_to(title_model_3, "解禁")
+nearest_to(title_model_1, "開発")
+nearest_to(title_model_2, "開発")
+nearest_to(title_model_3, "開発")
+nearest_to(title_model_1, "上昇")
+nearest_to(title_model_2, "上昇")
+nearest_to(title_model_3, "上昇")
 # 人名
-nearest_to(title_model_v10_w15, "しみけん")
-nearest_to(title_model_v10_w50, "しみけん")
-nearest_to(title_model_v100_w15, "しみけん")
-nearest_to(title_model_v100_w50, "しみけん")
-nearest_to(title_model_v10_w15, "あべみかこ")
-nearest_to(title_model_v10_w50, "あべみかこ")
-nearest_to(title_model_v100_w15, "あべみかこ")
-nearest_to(title_model_v100_w50, "あべみかこ")
-nearest_to(title_model_v10_w15, "浅田結梨")
-nearest_to(title_model_v10_w50, "浅田結梨")
-nearest_to(title_model_v100_w15, "浅田結梨")
-nearest_to(title_model_v100_w50, "浅田結梨")
+mat_1 <- rbind(
+  queryToMat(word = "しみけん", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "しみけん", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "しみけん", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "しみけん", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+mat_1 <- rbind(
+  queryToMat(word = "あべみかこ", window_size = c(5, 15, 30), vector_size = c(10, 10, 10))
+  ,queryToMat(word = "あべみかこ", window_size = c(5, 15, 30), vector_size = c(100, 100, 100))
+  ,queryToMat(word = "あべみかこ", window_size = c(5, 15, 30), vector_size = c(300, 300, 300))
+  ,queryToMat(word = "あべみかこ", window_size = c(5, 15, 30), vector_size = c(500, 500, 500))
+)
+View(mat_1)
+nearest_to(title_model_1, "しみけん")
+nearest_to(title_model_2, "しみけん")
+nearest_to(title_model_3, "しみけん")
+nearest_to(title_model_1, "あべみかこ")
+nearest_to(title_model_2, "あべみかこ")
+nearest_to(title_model_3, "あべみかこ")
+nearest_to(title_model_1, "浅田結梨")
+nearest_to(title_model_2, "浅田結梨")
+nearest_to(title_model_3, "浅田結梨")
 # 多義的な言葉
-nearest_to(title_model_v10_w15, "百合")
-nearest_to(title_model_v10_w50, "百合")
-nearest_to(title_model_v100_w15, "百合")
-nearest_to(title_model_v100_w50, "百合")
-nearest_to(title_model_v10_w15, "菊")
-nearest_to(title_model_v10_w50, "菊")
-nearest_to(title_model_v100_w15, "菊")
-nearest_to(title_model_v100_w50, "菊")
+nearest_to(title_model_1, "百合")
+nearest_to(title_model_2, "百合")
+nearest_to(title_model_3, "百合")
+nearest_to(title_model_1, "菊")
+nearest_to(title_model_2, "菊")
+nearest_to(title_model_3, "菊")
 
 # 演算
-nearest_to(title_model_v10_w15, title_model_v10_w15[["あべみかこ"]] - title_model_v10_w15[["貧乳"]])
-nearest_to(title_model_v10_w50, title_model_v10_w50[["あべみかこ"]] - title_model_v10_w50[["貧乳"]])
-nearest_to(title_model_v100_w15, title_model_v100_w15[["あべみかこ"]] - title_model_v100_w15[["貧乳"]])
-nearest_to(title_model_v100_w50, title_model_v100_w50[["あべみかこ"]] - title_model_v100_w50[["貧乳"]])
-nearest_to(title_model_v10_w15, title_model_v10_w15[["跡美しゅり"]] - title_model_v10_w15[["貧乳"]])
-nearest_to(title_model_v10_w50, title_model_v10_w50[["跡美しゅり"]] - title_model_v10_w50[["貧乳"]])
-nearest_to(title_model_v100_w15, title_model_v100_w15[["跡美しゅり"]] - title_model_v100_w15[["貧乳"]])
-nearest_to(title_model_v100_w50, title_model_v100_w50[["跡美しゅり"]] - title_model_v100_w50[["貧乳"]])
-nearest_to(title_model_v10_w15, title_model_v10_w15[["あべみかこ"]] + title_model_v10_w15[["巨乳"]])
-nearest_to(title_model_v10_w50, title_model_v10_w50[["あべみかこ"]] + title_model_v10_w50[["巨乳"]])
-nearest_to(title_model_v100_w15, title_model_v100_w15[["あべみかこ"]] + title_model_v100_w15[["巨乳"]])
-nearest_to(title_model_v100_w50, title_model_v100_w50[["あべみかこ"]] + title_model_v100_w50[["巨乳"]])
-nearest_to(title_model_v10_w15, title_model_v10_w15[["百合華"]] - title_model_v10_w15[["眼鏡"]])
-nearest_to(title_model_v10_w50, title_model_v10_w50[["百合華"]] - title_model_v10_w50[["眼鏡"]])
-nearest_to(title_model_v100_w15, title_model_v100_w15[["百合華"]] - title_model_v100_w15[["眼鏡"]])
-nearest_to(title_model_v100_w50, title_model_v100_w50[["百合華"]] - title_model_v100_w50[["眼鏡"]])
+# ちょっとした関数定義
+queryToMat2 <- function(word, window_size, vector_size, isPlus){
+  title_model_1 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[1],"_w", window_size[1],".txt"))
+  title_model_2 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[2],"_w", window_size[2],".txt"))
+  title_model_3 <- read.binary.vectors(filename = paste0("./model/model_titles_v", vector_size[3],"_w", window_size[3],".txt"))
+  
+  tmp_mat <- matrix(nrow = 10, ncol = 6)
+  if(isPlus){
+    tmp_mat[, 1] <- nearest_to(title_model_1, title_model_1[[word[1]]] + title_model_1[[word[2]]]) %>% names()
+    tmp_mat[, 2] <- nearest_to(title_model_1, title_model_1[[word[1]]] + title_model_1[[word[2]]]) %>% unlist() %>% round(digits = 2)
+    tmp_mat[, 3] <- nearest_to(title_model_2, title_model_2[[word[1]]] + title_model_2[[word[2]]]) %>% names()
+    tmp_mat[, 4] <- nearest_to(title_model_2, title_model_2[[word[1]]] + title_model_2[[word[2]]]) %>% unlist() %>% round(digits = 2)
+    tmp_mat[, 5] <- nearest_to(title_model_3, title_model_3[[word[1]]] + title_model_3[[word[2]]]) %>% names()
+    tmp_mat[, 6] <- nearest_to(title_model_3, title_model_3[[word[1]]] + title_model_3[[word[2]]]) %>% unlist() %>% round(digits = 2)
+  }else{
+    tmp_mat[, 1] <- nearest_to(title_model_1, title_model_1[[word[1]]] - title_model_1[[word[2]]]) %>% names()
+    tmp_mat[, 2] <- nearest_to(title_model_1, title_model_1[[word[1]]] - title_model_1[[word[2]]]) %>% unlist() %>% round(digits = 2)
+    tmp_mat[, 3] <- nearest_to(title_model_2, title_model_2[[word[1]]] - title_model_2[[word[2]]]) %>% names()
+    tmp_mat[, 4] <- nearest_to(title_model_2, title_model_2[[word[1]]] - title_model_2[[word[2]]]) %>% unlist() %>% round(digits = 2)
+    tmp_mat[, 5] <- nearest_to(title_model_3, title_model_3[[word[1]]] - title_model_3[[word[2]]]) %>% names()
+    tmp_mat[, 6] <- nearest_to(title_model_3, title_model_3[[word[1]]] - title_model_3[[word[2]]]) %>% unlist() %>% round(digits = 2)
+  }
+  return(rbind(c("w=5", "", "w=15", "", "w=30", ""), rep(x = c("word", "distance"), 3), tmp_mat))
+}
+mat_1 <- rbind(
+  queryToMat2(word = c("あべみかこ", "貧乳"), window_size = c(5, 15, 30)
+             , vector_size = c(100, 100, 100), isPlus = F)
+  ,queryToMat2(word = c("あべみかこ", "貧乳"), window_size = c(5, 15, 30)
+               , vector_size = c(300, 300, 300), isPlus = F)
+)
+View(mat_1)
+nearest_to(title_model_1, title_model_1[["あべみかこ"]] - title_model_1[["貧乳"]])
+nearest_to(title_model_2, title_model_2[["あべみかこ"]] - title_model_2[["貧乳"]])
+nearest_to(title_model_3, title_model_3[["あべみかこ"]] - title_model_3[["貧乳"]])
+nearest_to(title_model_1, title_model_1[["跡美しゅり"]] - title_model_1[["貧乳"]])
+nearest_to(title_model_2, title_model_2[["跡美しゅり"]] - title_model_2[["貧乳"]])
+nearest_to(title_model_3, title_model_3[["跡美しゅり"]] - title_model_3[["貧乳"]])
+nearest_to(title_model_1, title_model_1[["あべみかこ"]] + title_model_1[["巨乳"]])
+nearest_to(title_model_2, title_model_2[["あべみかこ"]] + title_model_2[["巨乳"]])
+nearest_to(title_model_3, title_model_3[["あべみかこ"]] + title_model_3[["巨乳"]])
+nearest_to(title_model_1, title_model_1[["百合華"]] - title_model_1[["眼鏡"]])
+nearest_to(title_model_2, title_model_2[["百合華"]] - title_model_2[["眼鏡"]])
+nearest_to(title_model_3, title_model_3[["百合華"]] - title_model_3[["眼鏡"]])
+
+nearest_to(title_model_1, title_model_1[["あべみかこ"]] + title_model_1[["眼鏡"]])
+nearest_to(title_model_2, title_model_2[["あべみかこ"]] + title_model_2[["眼鏡"]])
+nearest_to(title_model_3, title_model_3[["あべみかこ"]] + title_model_3[["眼鏡"]])
+
+nearest_to(title_model_1, title_model_1[["あべみかこ"]] + title_model_1[["制服"]])
+nearest_to(title_model_2, title_model_2[["あべみかこ"]] + title_model_2[["制服"]])
+nearest_to(title_model_3, title_model_3[["あべみかこ"]] + title_model_3[["制服"]])
+
+nearest_to(title_model_1, title_model_1[["眼鏡"]] + title_model_1[["制服"]])
+nearest_to(title_model_2, title_model_2[["眼鏡"]] + title_model_2[["制服"]])
+nearest_to(title_model_3, title_model_3[["眼鏡"]] + title_model_3[["制服"]])
 
 
-# 結果を見てみる------
-name_model_v10_w15 <- read.binary.vectors(filename = "./model/model_names_v10_w15.txt")
-name_model_v10_w50 <- read.binary.vectors(filename = "./model/model_names_v10_w50.txt")
-name_model_v100_w15 <- read.binary.vectors(filename = "./model/model_names_v100_w15.txt")
-name_model_v100_w50 <- read.binary.vectors(filename = "./model/model_names_v100_w50.txt")
+nearest_to(title_model_1, title_model_1[["眼鏡"]] + title_model_1[["地味"]])
+nearest_to(title_model_2, title_model_2[["眼鏡"]] + title_model_2[["地味"]])
+nearest_to(title_model_3, title_model_3[["眼鏡"]] + title_model_3[["地味"]])
 
-# 名詞
-nearest_to(name_model_v10_w15, "あべみかこ")
-nearest_to(name_model_v10_w50, "あべみかこ")
-nearest_to(name_model_v100_w15, "あべみかこ")
-nearest_to(name_model_v100_w50, "あべみかこ")
-nearest_to(name_model_v10_w15, "天使もえ")
-nearest_to(name_model_v10_w50, "天使もえ")
-nearest_to(name_model_v100_w15, "天使もえ")
-nearest_to(name_model_v100_w50, "天使もえ")
-nearest_to(name_model_v10_w15, "水野朝陽")
-nearest_to(name_model_v10_w50, "水野朝陽")
-nearest_to(name_model_v100_w15, "水野朝陽")
-nearest_to(name_model_v100_w50, "水野朝陽")
-nearest_to(name_model_v10_w15, "ANRI")
-nearest_to(name_model_v10_w50, "ANRI")
-nearest_to(name_model_v100_w15, "ANRI")
-nearest_to(name_model_v100_w50, "ANRI")
+nearest_to(title_model_1, title_model_1[["眼鏡"]] + title_model_1[["巨乳"]])
+nearest_to(title_model_2, title_model_2[["眼鏡"]] + title_model_2[["巨乳"]])
+nearest_to(title_model_3, title_model_3[["眼鏡"]] + title_model_3[["巨乳"]])
 
+nearest_to(title_model_1, title_model_1[["澁谷果歩"]] - title_model_1[["巨乳"]])
+nearest_to(title_model_2, title_model_2[["澁谷果歩"]] - title_model_2[["巨乳"]])
+nearest_to(title_model_3, title_model_3[["澁谷果歩"]] - title_model_3[["巨乳"]])
 
+nearest_to(title_model_1, title_model_1[["澁谷果歩"]] + title_model_1[["眼鏡"]])
+nearest_to(title_model_2, title_model_2[["澁谷果歩"]] + title_model_2[["眼鏡"]])
+nearest_to(title_model_3, title_model_3[["澁谷果歩"]] + title_model_3[["眼鏡"]])
